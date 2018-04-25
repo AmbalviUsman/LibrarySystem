d8b9a117b000c18fe245087bf04d948f56d08b51
Usman Ashraf <ambalviusman@gmail.com>
2018-04-25 17:20:01 +0500

    Main RunLibrarySystem file.

---
+++ src/library/RunLibrarySystem.java
@@ -0,0 +1,13 @@
+package library;
+
+import library.controller.LibrarySystem;
+
+public class RunLibrarySystem {
+
+	public static void main(String[] args){
+		
+		new LibrarySystem();
+		
+	}
+	
+}
